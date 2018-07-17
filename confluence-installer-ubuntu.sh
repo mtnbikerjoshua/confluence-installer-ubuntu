9e50fa48d743d3d9e6e3f90806c8087cbac259c2
Joshua Carpenter <mtnbikerjoshua@gmail.com>
2018-07-16 19:07:12 -0600

    Update confluence-installer-ubuntu.sh

--- confluence-installer-ubuntu.sh
+++ confluence-installer-ubuntu.sh
@@ -17,11 +17,11 @@ rm mysql-connector-java-5.1.46.tar.gz
 mv mysql-connector-java-5.1.46/mysql-connector-java-5.1.46.jar /opt/atlassian/confluence/confluence/WEB-INF/lib/mysql-connector-java-5.1.46.jar
 rm -r mysql-connector-java-5.1.46
 /etc/init.d/confluence restart
-echo "You should now type yourserverip:8090 in your browser's address bar and set up confluence"
+echo "You should now type yourserverip:8090 in your browser's address bar and set up confluence. When you get to the \"Set up your database\" step, choose \"My own database\" and then enter the following options:"
 echo "Database type: MySQL"
-echo "Connection: Simple"
+echo "Setup type: Simple"
 echo "Hostname: localhost"
 echo "Port: 3306"
-echo "Database Name: confluence"
+echo "Database name: confluence"
 echo "Username: confluenceuser"
-echo "Password: What ever you added to the installation file"
+echo "Password: Whatever you added to the mysqldatacreation.sql file (If you didn't change this, your password is InsertPasswordHere"
