d6cd593e799b01410481ca838be312e294362d4d
Joshua Carpenter <mtnbikerjoshua@gmail.com>
2018-07-16 18:44:09 -0600

    Update mysqldatacreation.sql

--- mysqldatacreation.sql
+++ mysqldatacreation.sql
@@ -1,2 +1,2 @@
 CREATE DATABASE confluence CHARACTER SET utf8 COLLATE utf8_bin;
-GRANT ALL PRIVILEGES ON confluence.* TO 'confluenceuser'@'localhost' IDENTIFIED BY 'CeSzs1NPtP5Y26f';
+GRANT ALL PRIVILEGES ON confluence.* TO 'confluenceuser'@'localhost' IDENTIFIED BY 'InsertPasswordHere';
